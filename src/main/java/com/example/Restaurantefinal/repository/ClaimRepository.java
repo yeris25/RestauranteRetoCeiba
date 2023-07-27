77d1ae7378e366a576209cf1a1fcaa185ea63d9f
smarinv16 <funemesanchez@gmail.com>
2023-07-27 09:24:42 -0500

    Se inicializa reto

--- src/main/java/com/example/Restaurantefinal/repository/ClaimRepository.java
+++ src/main/java/com/example/Restaurantefinal/repository/ClaimRepository.java
@@ -4,6 +4,7 @@ import com.example.Restaurantefinal.entity.Claim;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.*;
 import java.awt.*;
 
 @Repository
--- src/main/java/com/example/Restaurantefinal/repository/MenuRepository.java
+++ src/main/java/com/example/Restaurantefinal/repository/MenuRepository.java
@@ -1,10 +1,14 @@
 package com.example.Restaurantefinal.repository;
 
+import com.example.Restaurantefinal.entity.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.*;
+
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    Page<Menu> findByCategoryAndCampus(String category, String campus, Pageable pager);
 }
